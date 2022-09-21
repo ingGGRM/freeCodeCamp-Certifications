eef1e9fab8b586eb3bd65f71bdb2e12251f0c2c1
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-09-21 09:15:34 -0400

    Certification3: Markdown Previewer - Imported Marked Library propperly. Added div element to the reder function with the dangerouslySetInnerHTML and set to used the state input variable

--- frontEnd-development-libraries/markdown-previewer/src/index.js
+++ frontEnd-development-libraries/markdown-previewer/src/index.js
@@ -1,36 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-let marked = require("marked"); //import marked library
+import { marked } from 'marked'; // Imported marked library propperly
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			input: ""
+			input: "# Hello",
 		};
 
 		this.inputHandler = this.inputHandler.bind(this);
 	}
 
 	inputHandler(event) {
-		this.setState(state => ({
+		this.setState({
 			input: event.target.value
-		}));
+		});
 	}
 
 	render() {
 		return (
 			<div className="App">
 				<textarea id="editor" value={this.state.input} onChange={this.inputHandler}></textarea>
-				<div id="preview">
-					{/*
-					dangerouslySetInnerHTML={{
-                  __html: marked(this.state.input),
-				  }}
-				*/}
+				<div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.input)}}>
 				</div>
-			</div>
+			</div>	
 		);
 	}
 }
