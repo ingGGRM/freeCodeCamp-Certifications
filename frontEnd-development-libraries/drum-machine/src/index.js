88de93593ecfc8a2696acc7e19942911ce94ab7a
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-09-29 15:39:25 -0400

    Certification3: Drum Machine - Changed code structure, now using a component to generate each button. Fixed a CSS text transformation that was causing freeCodeCamp test to fail

--- frontEnd-development-libraries/drum-machine/src/index.js
+++ frontEnd-development-libraries/drum-machine/src/index.js
@@ -14,73 +14,165 @@ import heater4 from "./sounds/Heater-4_1.mp3";
 import heater5 from "./sounds/Heater-6.mp3";
 //**********************************************//
 
-class App extends React.Component {
+class DrumPad extends React.Component {
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			displayMsg: "Drum Machine by ingGGRM"
-		};
-
-		this.sounds = [
-			{name: "Heater 1", sound: new Audio(heater1), keySymbol: 'Q', keyCode: 81},
-			{name: "Heater 2", sound: new Audio(heater2), keySymbol: 'W', keyCode: 87},
-			{name: "Heater 3", sound: new Audio(heater3), keySymbol: 'E', keyCode: 69},
-			{name: "Heater 4", sound: new Audio(heater4), keySymbol: 'A', keyCode: 65},
-			{name: "Heater 5", sound: new Audio(heater5), keySymbol: 'S', keyCode: 83},
-			{name: "Kick", sound: new Audio(kick), keySymbol: 'D', keyCode: 68},
-			{name: "Plates 1", sound: new Audio(plates_1), keySymbol: 'Z', keyCode: 90},
-			{name: "Plates 2", sound: new Audio(plates_2), keySymbol: 'X', keyCode: 88},
-			{name: "Plates 3", sound: new Audio(plates_3), keySymbol: 'C', keyCode: 67},
-		];
 
 		this.keyPressHandler = this.keyPressHandler.bind(this);
 		this.clickHandler = this.clickHandler.bind(this);
 		this.player = this.player.bind(this);
 	}
 
-	keyPressHandler = (event) => {
-		console.log(event.keyCode);
-		this.sounds.forEach(sound => {
-			if(sound.keyCode === event.keyCode) {
-				this.player(sound);
-				return;
-			}
-		});
+	componentDidMount() {
+		document.addEventListener("keydown", this.keyPressHandler);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener("keydown", this.keyPressHandler);
+	}
+
+	keyPressHandler(e) {
+		if ([65, 67, 68, 69, 81, 83, 87, 88, 90].includes(e.keyCode)) {
+			this.player(
+				document.getElementById(String.fromCharCode(e.keyCode)),
+				this.props.soundData.name
+			);
+		}
 	}
 
 	clickHandler(event) {
 		console.log(event.target.value);
-		this.sounds.forEach(sound => {
-			if(sound.keyCode === Number(event.target.value)) {
-				this.player(sound);
-				return;
-			}
-		});
+		this.player(
+			document.getElementById(String.fromCharCode(event.target.value)),
+			this.props.soundData.name
+		);
 	}
 
-	player(snd) {
-		console.log(`Playing: ${snd.name}`);
+	player(snd, text) {
+		console.log(`Playing: ${text}`);
 
-		this.setState(state => ({
-			displayMsg: `Playing: ${snd.name}`
+		this.props.stateChanger(text);
+
+		snd.currentTime = 0;
+		snd.play();
+	}
+
+	render() {
+		console.log(this.props.soundData);
+		return (
+			<button
+				className="drum-pad"
+				id={this.props.soundData.ident}
+				onClick={this.clickHandler}
+				value={this.props.soundData.keyCode}
+			>
+				{this.props.soundData.keySymbol}
+				<audio
+					src={this.props.soundData.sound}
+					id={this.props.soundData.keySymbol}
+					className="clip"
+				></audio>
+			</button>
+		);
+	}
+}
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			displayMsg: "Drum Machine by ingGGRM",
+		};
+
+		this.soundsData = [
+			{
+				name: "Heater 1",
+				sound: heater1,
+				keySymbol: "Q",
+				keyCode: 81,
+				ident: "heaterSoft",
+			},
+			{
+				name: "Heater 2",
+				sound: heater2,
+				keySymbol: "W",
+				keyCode: 87,
+				ident: "heaterMedium",
+			},
+			{
+				name: "Heater 3",
+				sound: heater3,
+				keySymbol: "E",
+				keyCode: 69,
+				ident: "heaterHard",
+			},
+			{
+				name: "Heater 4",
+				sound: heater4,
+				keySymbol: "A",
+				keyCode: 65,
+				ident: "heaterHarder",
+			},
+			{
+				name: "Heater 5",
+				sound: heater5,
+				keySymbol: "S",
+				keyCode: 83,
+				ident: "heaterHardest",
+			},
+			{
+				name: "Kick",
+				sound: kick,
+				keySymbol: "D",
+				keyCode: 68,
+				ident: "simpleKick",
+			},
+			{
+				name: "Plates 1",
+				sound: plates_1,
+				keySymbol: "Z",
+				keyCode: 90,
+				ident: "platesSoft",
+			},
+			{
+				name: "Plates 2",
+				sound: plates_2,
+				keySymbol: "X",
+				keyCode: 88,
+				ident: "platesMedium",
+			},
+			{
+				name: "Plates 3",
+				sound: plates_3,
+				keySymbol: "C",
+				keyCode: 67,
+				ident: "platesHard",
+			},
+		];
+
+		this.stateChanger = this.stateChanger.bind(this);
+	}
+
+	stateChanger(text) {
+		this.setState((state) => ({
+			displayMsg: `Playing: ${text}`,
 		}));
-
-		snd.sound.currentTime = 0;
-		snd.sound.play();
 	}
 
 	render() {
 		return (
-			<div id="drum-machine" onKeyDown={this.keyPressHandler} tabIndex="-1" >
+			<div id="drum-machine">
 				<h1 id="title">This is the begining</h1>
 				<div id="side-panel">
 					<div id="display">{this.state.displayMsg}</div>
 				</div>
 				<div id="pad-container">
-					{
-						this.sounds.map(snd => <button className="drum-pad" onClick={this.clickHandler} value={snd.keyCode} >{snd.name} <br /> <spam>{snd.keySymbol}</spam></button>)
-					}
+					{this.soundsData.map((soundData) => (
+						<DrumPad
+							soundData={soundData}
+							stateChanger={this.stateChanger}
+						></DrumPad>
+					))}
 				</div>
 			</div>
 		);
