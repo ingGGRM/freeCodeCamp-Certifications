e3bb8316b6e7e1923f09a11cbb69e9e4d127cb1e
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-09-30 11:56:13 -0400

    Certification3: JavaScript Calculator - Added main project folder and files

---
+++ frontEnd-development-libraries/javascript-calculator/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return;
+	}
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
