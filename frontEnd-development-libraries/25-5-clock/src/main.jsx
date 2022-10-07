273273cb1fe7b47a5d73b8582a391805d5375ff2
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-10-07 13:39:44 -0400

    Certification3: 25 + 5 Clock  - Started creation of page elements. Added Button elements with FontAweson icons inside. Fixed click on buttons events pointed to Fas element instead of button. Added pointerEvents=none to Fas elements

--- frontEnd-development-libraries/25-5-clock/src/main.jsx
+++ frontEnd-development-libraries/25-5-clock/src/main.jsx
@@ -3,25 +3,53 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 /**********************************************************************************************/
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
-import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowAltCircleUp } from "@fortawesome/free-solid-svg-icons";
 
-const arrowDown = <FontAwesomeIcon icon={faAngleDoubleDown} />
-const arrowUp = <FontAwesomeIcon icon={faAngleDoubleUp} />
+const arrowDown = <FontAwesomeIcon icon={faArrowAltCircleDown} pointerEvents="none" />;
+const arrowUp = <FontAwesomeIcon icon={faArrowAltCircleUp} pointerEvents="none" />;
 /**********************************************************************************************/
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			break: 5,
+			session: 25,
+			counter: 0,
+		};
+
+		this.clickHandler = this.clickHandler.bind(this);
+	}
+
+	clickHandler(event) {
+		console.log(event.target.value);
 	}
 
 	render() {
 		return (
 			<div>
-				{arrowDown}{arrowUp}
+				<label id="title">25+5 Clock</label>
+				<div id="settings-panel">
+					<div id="break-panel">
+						<label id="break-label">
+							Break Length:
+							<span id="break-length">{` ${this.state.break}`}</span>
+						</label>
+						<button id="break-decrement" onClick={this.clickHandler} value="breakDown">{arrowDown}</button>
+						<button id="break-increment" onClick={this.clickHandler} value="breakUp">{arrowUp}</button>
+					</div>
+					<div id="session-panel">
+						<label id="session-label">
+							Session Length:
+							<span id="session-length">{` ${this.state.session}`}</span>
+							<button id="session-decrement" onClick={this.clickHandler} value="sessionDown">{arrowDown}</button>
+							<button id="session-increment" onClick={this.clickHandler} value="sessionUp">{arrowUp}</button>
+						</label>
+					</div>
+				</div>
 			</div>
 		);
 	}
