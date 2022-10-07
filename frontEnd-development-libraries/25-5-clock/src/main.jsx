62a3706611c3c90e57bc4073e17b9cd627dad149
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-10-07 14:12:40 -0400

    Certification3: 25 + 5 Clock  - Started creation of page elements. Added clickHandler function to setState data as needed

--- frontEnd-development-libraries/25-5-clock/src/main.jsx
+++ frontEnd-development-libraries/25-5-clock/src/main.jsx
@@ -7,8 +7,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
 import { faArrowAltCircleUp } from "@fortawesome/free-solid-svg-icons";
 
-const arrowDown = <FontAwesomeIcon icon={faArrowAltCircleDown} pointerEvents="none" />;
-const arrowUp = <FontAwesomeIcon icon={faArrowAltCircleUp} pointerEvents="none" />;
+const arrowDown = (
+	<FontAwesomeIcon icon={faArrowAltCircleDown} pointerEvents="none" />
+);
+const arrowUp = (
+	<FontAwesomeIcon icon={faArrowAltCircleUp} pointerEvents="none" />
+);
 /**********************************************************************************************/
 
 class App extends React.Component {
@@ -18,14 +22,50 @@ class App extends React.Component {
 		this.state = {
 			break: 5,
 			session: 25,
-			counter: 0,
+			timer: "",
 		};
 
 		this.clickHandler = this.clickHandler.bind(this);
 	}
 
+	componentDidMount() {
+		this.setState({ timer: `${this.state.session}:00` });
+	}
+
 	clickHandler(event) {
 		console.log(event.target.value);
+
+		let updBreak = this.state.break,
+			updSession = this.state.session;
+
+		switch (event.target.value) {
+			case "breakDown":
+				updBreak =
+					this.state.break === 1
+						? this.state.break
+						: this.state.break - 1;
+				break;
+			case "breakUp":
+				updBreak += 1;
+				break;
+			case "sessionDown":
+				updSession =
+					this.state.session === 1
+						? this.state.session
+						: this.state.session - 1;
+				break;
+			case "sessionUp":
+				updSession = this.state.session + 1;
+				break;
+			default:
+				break;
+		}
+
+		this.setState((state) => ({
+			break: updBreak,
+			session: updSession,
+			timer: `${updSession}:00`,
+		}));
 	}
 
 	render() {
@@ -38,16 +78,44 @@ class App extends React.Component {
 							Break Length:
 							<span id="break-length">{` ${this.state.break}`}</span>
 						</label>
-						<button id="break-decrement" onClick={this.clickHandler} value="breakDown">{arrowDown}</button>
-						<button id="break-increment" onClick={this.clickHandler} value="breakUp">{arrowUp}</button>
+						<button
+							id="break-decrement"
+							onClick={this.clickHandler}
+							value="breakDown"
+						>
+							{arrowDown}
+						</button>
+						<button
+							id="break-increment"
+							onClick={this.clickHandler}
+							value="breakUp"
+						>
+							{arrowUp}
+						</button>
 					</div>
 					<div id="session-panel">
 						<label id="session-label">
 							Session Length:
 							<span id="session-length">{` ${this.state.session}`}</span>
-							<button id="session-decrement" onClick={this.clickHandler} value="sessionDown">{arrowDown}</button>
-							<button id="session-increment" onClick={this.clickHandler} value="sessionUp">{arrowUp}</button>
+							<button
+								id="session-decrement"
+								onClick={this.clickHandler}
+								value="sessionDown"
+							>
+								{arrowDown}
+							</button>
+							<button
+								id="session-increment"
+								onClick={this.clickHandler}
+								value="sessionUp"
+							>
+								{arrowUp}
+							</button>
 						</label>
+					</div>
+					<div id="display">
+						<label id="timer-label">Session</label>
+						<label id="time-left">{this.state.timer}</label>
 					</div>
 				</div>
 			</div>
