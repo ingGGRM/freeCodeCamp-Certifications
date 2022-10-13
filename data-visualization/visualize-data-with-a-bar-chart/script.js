554157cf6dd009d28f25b1916487e75d95d90a4a
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-10-13 15:48:13 -0400

    Certification4: Visualize Data with a Bar Chart - Added code to change bars and show a tooltip on mouseover bars event. Created tooltip and added style to be hidden until wanted. Have to change tooltip position to be near the selected rect on the svg graphic

--- data-visualization/visualize-data-with-a-bar-chart/script.js
+++ data-visualization/visualize-data-with-a-bar-chart/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	req.onload = function () {
 		const json = JSON.parse(req.responseText); // convert string to object
 		console.log(json); // uncomment to print json file directly as an object
-		console.log(new Date(json.data[0][0]))
+		console.log(new Date(json.data[0][0]));
 
 		// set title element text from json
 		title.text(`${json.source_name} : ${json.code}`);
@@ -29,8 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		const xScale = d3
 			.scaleTime() // set scale type to time, becouse of dates values read from index 0 of data
 			.domain([
-				d3.min(json.data, (d) => new Date(d[0])),// set domain based on date data from json.data[0]
-				d3.max(json.data, (d) => new Date(d[0])),// set domain based on date data from json.data[0]
+				d3.min(json.data, (d) => new Date(d[0])), // set domain based on date data from json.data[0]
+				d3.max(json.data, (d) => new Date(d[0])), // set domain based on date data from json.data[0]
 			])
 			.range([padding, w - padding]);
 		const yScale = d3
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			.range([h - padding, padding]);
 
 		// create x and y axis
-		const xAxis = d3.axisBottom(xScale);//.tickFormat(d3.format("d")); // quit comma thousands separator in values format
+		const xAxis = d3.axisBottom(xScale); //.tickFormat(d3.format("d")); // quit comma thousands separator in values format
 		const yAxis = d3.axisLeft(yScale);
 
 		// draw x and y axis
@@ -51,9 +51,24 @@ document.addEventListener("DOMContentLoaded", function () {
 			.attr("id", "y-axis")
 			.attr("transform", "translate(" + padding + ", 0)")
 			.call(yAxis);
-		
+
 		// select tick elements to add tick class to them
 		d3.selectAll("ticks").attr("class", "tick");
+
+		// create toolTip elements that will show data of bar on mouseover event
+		let tooltip = d3
+			.select("body")
+			.append("div")
+			.attr("id", "tooltip")
+			.style("position", "absolute")
+			.style("visibility", "hidden")
+			.style("z-index", "10")
+			.style("background", "#000")
+			.style("width", "200px")
+			.style("height", "70px")
+			.style("padding", "5px")
+			.style("color", "white")
+			.style("transition-duration", "100ms");
 
 		// draw rects for each data entry
 		svg.selectAll("rect")
@@ -63,12 +78,27 @@ document.addEventListener("DOMContentLoaded", function () {
 			.attr("class", "bar")
 			.attr("x", (d) => xScale(new Date(d[0]))) // set rect on date data from json.data[0]
 			.attr("y", (d) => yScale(d[1]))
-			.attr("width", 1)
+			.attr("width", 2)
 			.attr("height", (d) => h - padding - yScale(d[1]))
-			.attr("fill", "black")
-			.attr("data-height", (d) => d[0])
-			.attr("data-gdp", (d) => d[1]);
-			//(d) => yScale(d[1])
+			.attr("fill", "orangered")
+			.attr("data-date", (d) => d[0])
+			.attr("data-gdp", (d) => d[1])
+			.style("transition-duration", "50ms")
+			.on("mouseover", (e) => {
+				// add mouseover event to bars
+				tooltip
+					.style("top", "200px")
+					.style("left", "200px")
+					.style("visibility", "visible")
+					.text(`Date: ${e[0]}\n GDP: ${e[1]}`)
+					.style("font-size", "20px"); // show tooltip
+			})
+			.on("mouseout", (e) => {
+				// add mouseout event to bars
+				tooltip.style("visibility", "hidden"); // hide tooltip
+			});
+
+		// show a toolTip on mouse over the svg element to show point data
 	};
 });
 
