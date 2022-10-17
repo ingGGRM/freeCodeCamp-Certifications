ad5fcb6d2f195fc5788801eaf0cd45ca9b24e624
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-10-17 16:09:34 -0400

    Certification4: Visualize Data with a Scatter Plot Graph - Added code to tray year data as real Years to generate the X axis. Yet working on making the sama work for the time mm:ss data to generate the Y axis

--- data-visualization/scatterplot-graph/script.js
+++ data-visualization/scatterplot-graph/script.js
@@ -4,7 +4,10 @@ const dataUrl =
 // event handler to work after page content has loaded
 document.addEventListener("DOMContentLoaded", function () {
 	const svg = d3.select("#graph"); // select #root element and store as svg
-	const tooltip = d3.select("#tooltip").attr("data-date", "").attr("data-gdp", 0);
+	const tooltip = d3
+		.select("#tooltip")
+		.attr("data-date", "")
+		.attr("data-gdp", 0);
 
 	const padding = 60;
 	const parentDiv = document.getElementById("root");
@@ -13,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	//set svg size
 	svg.attr("width", w).attr("height", h);
-    console.log(h, w);
+	console.log(h, w);
 
 	const req = new XMLHttpRequest();
 	req.open("GET", dataUrl, true);
@@ -21,42 +24,40 @@ document.addEventListener("DOMContentLoaded", function () {
 	req.onload = function () {
 		const json = JSON.parse(req.responseText); // convert string to object
 		console.log(json); // uncomment to print json file directly as an object
-        // The 3 aspects to take on account for graph construction
+		// The 3 aspects to take on account for graph construction
 		console.log(json[0].Year); // example getting object 0 Year
-        console.log(json[0].Time); // example getting object 0 Time
-        console.log(json[0].Doping); // if not "", then clasified as "Doping Allegation", else "Not Doping Allegation"
+		console.log(json[0].Time); // example getting object 0 Time
+		console.log(json[0].Doping); // if not "", then clasified as "Doping Allegation", else "Not Doping Allegation"
 
-		// set title element text from json
-//		title.text(`${json.source_name} : ${json.code}`);
-/*
+        // proccessing data formats to make it be like date (years, minutes, seconds, etc) data
+        let years = json.map(d => d3.timeParse("%Y")(d.Year));
+        let times = json.map(d => d3.timeParse("%M:%S")(d.Time));
+
 		// get x and y axis scales
 		const xScale = d3
-			.scaleTime() // set scale type to time, becouse of dates values read from index 0 of data
-			.domain([
-				d3.min(json.data, (d) => new Date(d[0])), // set domain based on date data from json.data[0]
-				d3.max(json.data, (d) => new Date(d[0])), // set domain based on date data from json.data[0]
-			])
+			.scaleTime()
+			.domain(d3.extent(years))
 			.range([padding, w - padding]);
 		const yScale = d3
-			.scaleLinear()
-			.domain([0, d3.max(json.data, (d) => d[1])])
+			.scaleTime() // set scale type to time, becouse of %M:%S values read from data array
+			.domain(d3.extent(times))
 			.range([h - padding, padding]);
 
 		// create x and y axis
-		const xAxis = d3.axisBottom(xScale); //.tickFormat(d3.format("d")); // quit comma thousands separator in values format
+		const xAxis = d3.axisBottom(xScale); // quit comma thousands separator in values format
 		const yAxis = d3.axisLeft(yScale);
 
 		// draw x and y axis
 		svg.append("g")
 			.attr("id", "x-axis")
 			.attr("transform", "translate(0, " + (h - padding) + ")")
-			.call(xAxis.ticks(d3.timeYear.every(5))); // set ticks to each 5 years
+			.call(xAxis).ticks(d3.timeYear.every(2));
 		svg.append("g")
 			.attr("id", "y-axis")
 			.attr("transform", "translate(" + padding + ", 0)")
 			.call(yAxis);
 
 		// select tick elements to add tick class to them
-		d3.selectAll("ticks").attr("class", "tick");*/
-    }
+		d3.selectAll("ticks").attr("class", "tick");
+	};
 });
