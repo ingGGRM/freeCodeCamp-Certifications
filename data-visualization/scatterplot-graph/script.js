55c44729669541906e9fc9a613d9d520070dbb38
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-10-17 15:13:52 -0400

    Certification4: Visualize Data with a Scatter Plot Graph - Added Title and DIV with SVG element for the graph creation. Added some CSS style to the whole document. Added raw data getter to the script.

---
+++ data-visualization/scatterplot-graph/script.js
@@ -0,0 +1,62 @@
+const dataUrl =
+	"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+// event handler to work after page content has loaded
+document.addEventListener("DOMContentLoaded", function () {
+	const svg = d3.select("#graph"); // select #root element and store as svg
+	const tooltip = d3.select("#tooltip").attr("data-date", "").attr("data-gdp", 0);
+
+	const padding = 60;
+	const parentDiv = document.getElementById("root");
+	const w = parentDiv.offsetWidth; // get container div width
+	const h = parentDiv.offsetHeight; // get container div height
+
+	//set svg size
+	svg.attr("width", w).attr("height", h);
+    console.log(h, w);
+
+	const req = new XMLHttpRequest();
+	req.open("GET", dataUrl, true);
+	req.send();
+	req.onload = function () {
+		const json = JSON.parse(req.responseText); // convert string to object
+		console.log(json); // uncomment to print json file directly as an object
+        // The 3 aspects to take on account for graph construction
+		console.log(json[0].Year); // example getting object 0 Year
+        console.log(json[0].Time); // example getting object 0 Time
+        console.log(json[0].Doping); // if not "", then clasified as "Doping Allegation", else "Not Doping Allegation"
+
+		// set title element text from json
+//		title.text(`${json.source_name} : ${json.code}`);
+/*
+		// get x and y axis scales
+		const xScale = d3
+			.scaleTime() // set scale type to time, becouse of dates values read from index 0 of data
+			.domain([
+				d3.min(json.data, (d) => new Date(d[0])), // set domain based on date data from json.data[0]
+				d3.max(json.data, (d) => new Date(d[0])), // set domain based on date data from json.data[0]
+			])
+			.range([padding, w - padding]);
+		const yScale = d3
+			.scaleLinear()
+			.domain([0, d3.max(json.data, (d) => d[1])])
+			.range([h - padding, padding]);
+
+		// create x and y axis
+		const xAxis = d3.axisBottom(xScale); //.tickFormat(d3.format("d")); // quit comma thousands separator in values format
+		const yAxis = d3.axisLeft(yScale);
+
+		// draw x and y axis
+		svg.append("g")
+			.attr("id", "x-axis")
+			.attr("transform", "translate(0, " + (h - padding) + ")")
+			.call(xAxis.ticks(d3.timeYear.every(5))); // set ticks to each 5 years
+		svg.append("g")
+			.attr("id", "y-axis")
+			.attr("transform", "translate(" + padding + ", 0)")
+			.call(yAxis);
+
+		// select tick elements to add tick class to them
+		d3.selectAll("ticks").attr("class", "tick");*/
+    }
+});
