2cc9920cb986b1eeb36567daa0145ae6ec4d5a0b
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-10-21 16:05:18 -0400

    Certification4: Visualize Data with a Choropleth Map - Added topojson script to html head, to used features mode to generate graph paths. Now counties are displaying on the SVG element. Used tranform to scale the graph. NOTE: Have to work with the fill color on each county that ar not filled yet. I think that the issue is that the Educational Data for the color last too much to get. Maybe d3 Queue is the answer so will be imported as a script in html next time.

--- data-visualization/choropleth-map/script.js
+++ data-visualization/choropleth-map/script.js
@@ -1,22 +1,13 @@
-const url = [
-	{
-		usEducation:
-			"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json",
-	},
-	{
-		usCounty:
-			"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json",
-	},
-];
-
 document.addEventListener("DOMContentLoaded", function () {
 	builder();
 });
 
 function builder() {
 	const container = document.getElementById("graph-container");
-	const w = d3.select(container).style("width"); // get container div width
-	const h = d3.select(container).style("height"); // get container div height;
+	//const w = d3.select(container).style("width"); // get container div width
+	//const h = d3.select(container).style("height"); // get container div height;
+	const w = 900;
+	const h = 425;
 	const paddingX = 30; // value to use as margin to the sides of the graph
 	const paddingY = 60; // value to use as margin to the sides of the graph
 
@@ -27,7 +18,7 @@ function builder() {
 		.attr("id", "graph")
 		// Responsive SVG needs these 2 attributes and no width and height attr.
 		.attr("preserveAspectRatio", "xMinYMin meet")
-		.attr("viewBox", `0 0 900 500`)
+		.attr("viewBox", `0 0 ${w} ${h}`)
 		//.attr("viewBox", `0 0 ${w.slice(0,-2)} ${h.slice(0,-2)}`)
 		//.attr("width", w)
 		//.attr("height", h)
@@ -35,10 +26,47 @@ function builder() {
 		.style("border", "1px solid black");
 
 	// Map and projection
-	let path = d3.geoPath();
+	const path = d3.geoPath();
 	let projection = d3
 		.geoMercator()
 		.scale(70)
 		.center([0, 20])
-		.translate([width / 2, height / 2]);
+		.translate([w / 2, h / 2]);
+
+	// Data and color scale
+	let dataSet = {};
+	const colorScale = d3
+		.scaleThreshold()
+		.domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+		.range(d3.schemeBlues[7]);
+
+	// Load external data and boot
+	d3.json(
+		"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json",
+		function (error, us) {
+			d3.json(
+				"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json",
+				function (data) {
+					data.map((d) => {
+						dataSet[d.fips] = d.bachelorsOrHigher;
+					});
+				}
+			);
+			// Draw the map
+			let feature = topojson.feature(us, us.objects.counties);
+			console.log(us);
+			console.log(dataSet);
+			svg.selectAll("path")
+				.data(feature.features)
+				.enter()
+				.append("path")
+				// draw each country
+				.attr("d", d3.geoPath())
+				.attr("transform", `scale(0.85, 0.7)`)
+				.attr("class", "county")
+				.attr("fill", function (d) {
+					return colorScale(dataSet[d.id]);
+				});
+		}
+	);
 }
