3ca0a6100074d87483dbdcf06d64d2ffe5955b0e
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-10-24 14:57:36 -0400

    Certification4: Visualize Data with a Choropleth Map - Added working color scale, so counties are now colored. Added legend and fixed map scale issue

--- data-visualization/choropleth-map/script.js
+++ data-visualization/choropleth-map/script.js
@@ -33,13 +33,6 @@ function builder() {
 		.center([0, 20])
 		.translate([w / 2, h / 2]);
 
-	// Data and color scale
-	let dataSet = {};
-	const colorScale = d3
-		.scaleThreshold()
-		.domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-		.range(d3.schemeBlues[7]);
-
 	// Load external data and boot
 	d3.json(
 		"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json",
@@ -47,26 +40,79 @@ function builder() {
 			d3.json(
 				"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json",
 				function (data) {
+					let dataSet = {};
+					// make a map with the data and the id of the counties, so can get the bachelorsOrHigher for an specific id
 					data.map((d) => {
 						dataSet[d.fips] = d.bachelorsOrHigher;
 					});
+
+					// Color scale using the max value of the dato to get 7 numbers for the 7 colors range
+					const colorScale = d3
+						.scaleThreshold()
+						.domain(
+							(function () {
+								let arr = [];
+								let max = d3.max(
+									data,
+									(d) => d.bachelorsOrHigher
+								);
+								for (let i = 0; i < 7; i++) {
+									arr.push((max / 7) * (i + 1));
+									// console.log(arr);
+								}
+
+								return arr;
+							})()
+						)
+						.range(d3.schemeReds[7]);
+
+					// create graph legend
+					const legend = svg
+						.append("g")
+						.attr("id", "legend")
+						.attr("width", "100px")
+						.attr("height", "100px")
+						.style("border", "1px solid green")
+						.style("fill", "black")
+
+					// Draw the map
+					let feature = topojson.feature(us, us.objects.counties);
+					// console.log(us);
+					// console.log(dataSet);
+					svg.selectAll("path")
+						.data(feature.features)
+						.enter()
+						.append("path")
+						// draw each country
+						.attr("d", d3.geoPath())
+						.attr("transform", `scale(0.85, 0.7)`)
+						.attr("class", "county")
+						.attr("fill", (d) => colorScale(dataSet[d.id]))
+						.attr("data-fips", (d) => d.id)
+						.attr("data-education", (d) => dataSet[d.id]);
+					
+					console.log(colorScale.range())
+
+					legend.selectAll("rect")
+						.data(colorScale.range())
+						.enter()
+						.append("rect")
+						.attr("x", "calc(100% - 30px)")
+						.attr("y", (d, i) => `${50 * (i + 1)}px`)
+						.attr("width", "15px")
+						.attr("height", "40px")
+						.attr("fill", d => d);
+
+					legend.selectAll("text")
+						.data(colorScale.domain())
+						.enter()
+						.append("text")
+						.attr("x", "calc(93%)")
+						.attr("y", (d, i) => `${75 + (i * 50)}px`)
+						.text(d => d.toFixed(2))
+						.style("font-size", "2vh");
 				}
 			);
-			// Draw the map
-			let feature = topojson.feature(us, us.objects.counties);
-			console.log(us);
-			console.log(dataSet);
-			svg.selectAll("path")
-				.data(feature.features)
-				.enter()
-				.append("path")
-				// draw each country
-				.attr("d", d3.geoPath())
-				.attr("transform", `scale(0.85, 0.7)`)
-				.attr("class", "county")
-				.attr("fill", function (d) {
-					return colorScale(dataSet[d.id]);
-				});
 		}
 	);
 }
